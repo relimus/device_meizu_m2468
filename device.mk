c552fd8b584c0c6f6b1e7d8553e281c3c9845e25
relimus <relimus@proton.me>
2026-03-07 18:20:14 +0800

    m2468: 再次完善 BoardConfig.mk & device.mk

--- device.mk
+++ device.mk
@@ -68,7 +68,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audioadsprpcd \
     android.hardware.audio.service \
-    audio.primary.kalama \
     sound_trigger.primary.kalama \
     libagm_compress_plugin \
     libagm_mixer_plugin \
