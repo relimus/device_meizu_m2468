c59d9dd092d3454e959bfede692a9926bff1f603
relimus <relimus@proton.me>
2026-03-08 15:11:34 +0800

    m2468: 为 vendor.libdpmframework.so 添加 libhidlbase_shim.so Unresolved symbol: _ZN7android8hardware6Parcel7setDataEPKhm

--- extract-files.py
+++ extract-files.py
@@ -87,6 +87,8 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_lockPlanes')
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock'),
+    'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
+        .add_needed('libhidlbase_shim.so'),
 }
 
 lib_fixups: lib_fixups_user_type = {
