3064c55117c896690ecda3cbcb3da61758597e74
relimus <relimus@proton.me>
2026-03-08 11:36:57 +0800

    m2468: 为 libspukeymint.so 添加 android.hardware.security.rkp-V3-ndk.so Unresolved symbol: _ZN4aidl7android8hardware8security7keymint30BnRemotelyProvisionedComponent12createBinderEv Unresolved symbol: _ZN4aidl7android8hardware8security7keymint30BnRemotelyProvisionedComponent16getInterfaceHashEPNSt3__112basic_stringIcNS5_11char_traitsIcEENS5_9allocatorIcEEEE Unresolved symbol: _ZN4aidl7android8hardware8security7keymint30BnRemotelyProvisionedComponent19getInterfaceVersionEPi Unresolved symbol: _ZN4aidl7android8hardware8security7keymint30BnRemotelyProvisionedComponentC2Ev Unresolved symbol: _ZN4aidl7android8hardware8security7keymint30BnRemotelyProvisionedComponentD2Ev

--- extract-files.py
+++ extract-files.py
@@ -69,6 +69,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbase_shim.so'),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
         .add_needed('qcodec2_shim.so'),
+    'vendor/lib64/libspukeymint.so': blob_fixup()
+        .add_needed('android.hardware.security.rkp-V3-ndk.so'),
 }
 
 lib_fixups: lib_fixups_user_type = {
