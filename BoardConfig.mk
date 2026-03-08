d750e3179a293446402486c7f27b0a8912e6756f
relimus <relimus@proton.me>
2026-03-08 20:22:25 +0800

    m2468: 设置 内核 SELinux 宽容

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,8 +78,21 @@ BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_BASE := 0x00000000
 
-BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 qcom_geni_serial.con_enabled=1 nosoftlockup page_poison=1 sysrq_always_enabled bootconfig buildvariant=user
-BOARD_BOOTCONFIG := androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 androidboot.load_modules_parallel=true androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE := \
+    video=vfb:640x400,bpp=32,memsize=3072000 \
+    qcom_geni_serial.con_enabled=1 \
+    nosoftlockup \
+    page_poison=1 \
+    sysrq_always_enabled \
+    bootconfig \
+    androidboot.selinux=permissive
+
+BOARD_BOOTCONFIG := \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.load_modules_parallel=true \
+    androidboot.console=ttyMSM0
 
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
