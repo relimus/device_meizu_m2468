c5bce47ebfc29ac82ef75ced0f40a8204a701548
relimus <relimus@proton.me>
2026-03-08 15:14:42 +0800

    m2468: 为 android.hardware.security.keymint-service-spu-qti 添加 android.hardware.security.rkp-V3-ndk.so Unresolved symbol: _ZN4aidl7android8hardware8security7keymint29IRemotelyProvisionedComponent10descriptorE

--- extract-files.py
+++ extract-files.py
@@ -89,7 +89,10 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_unlock'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
-    'vendor/bin/hw/android.hardware.security.keymint-service-qti': blob_fixup()
+    (
+    'vendor/bin/hw/android.hardware.security.keymint-service-qti',
+    'vendor/bin/hw/android.hardware.security.keymint-service-spu-qti',
+    ): blob_fixup()
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
 }
 
